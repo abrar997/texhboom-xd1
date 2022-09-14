248ff631dc8d89ccc2bb7862037fc51e05a9aab7
abrar997 <abraralrawi997.com>
2022-09-14 15:47:58 +0300

    home

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,7 +9,7 @@ const data=[
 ]
 
   return (
-    <div className="flex justify-center items-center bg-primary  border-2 border-footer fixed bottom-0 left-0 right-0">
+    <div className="flex justify-center items-center bg-primary  border-[1px] border-footer fixed bottom-0 left-0 right-0">
     {data.map((items,index)=>{
       return (
         <div key={index} className='m-4 rounded-[50%] text-xl  border-[#ECF0F3] text-primary  p-3 shadow-xl bg-icon '>
